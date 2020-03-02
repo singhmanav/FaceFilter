751535e25eab6ed9264bc198c13f0f363234d04a
Manav <manavmanuprakash@gmail.com>
2020-03-03 02:26:57 +0530

    Initial Commit

---
+++ FaceFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceFilter
+//
+//  Created by Manav on 03/03/20.
+//  Copyright © 2020 Manav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
